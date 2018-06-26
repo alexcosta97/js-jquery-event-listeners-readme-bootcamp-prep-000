4373d72e9da224a3b91a9593f80f9b6460699b7d
Alex Paulo da Costa <alex.callum.c@gmail.com>
2018-06-26 10:43:20 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,17 +1,20 @@
 //define functions here
 
 $(document).ready(function(){
+  getIt();
+  frameIt();
+});
+
+function frameIt()
+  {
+    $('img').on('load', function(){
+      $('img').addClass('tasty');
+    });
+  }
+  
   function getIt()
   {
     $('p').on('click', function(){
       alert('Hey!');
     })
   }
-  
-  function frameIt()
-  {
-    $('img').on('load', function(){
-      $('img').addClass('tasty');
-    });
-  }
-});
